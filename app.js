d9e4cabb71dd8cd0d80d9d9d5c8a4ac05e264abb
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-03-20 14:32:20 +0200

    Icon on marker changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { apiKey } from './apikey.js';
- 
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -25,33 +25,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //function to get info from apis
     function getData() {
-        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAdress}`).then(res => res.json()).then(data => {
-            ip.textContent = data.ip
-            place.textContent = data.location.region
-            time.textContent = data.location.timezone
-            internet.textContent = data.isp
-            getMap(data.location.lat, data.location.lng)
-            // console.log(`[${data.location.lat}, ${data.location.lng}]`)
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAdress}`)
+            .then(res => res.json())
+            .then(data => {
+            ip.textContent = data.ip;
+            place.textContent = data.location.region;
+            time.textContent = data.location.timezone;
+            internet.textContent = data.isp;
+            getMap(data.location.lat, data.location.lng);
         });
     };
 
     // function to get map location based on lat & long
     function getMap(latitude, longtitude) { 
+        //check if map is already initialized, if yes set it to null
         var container = L.DomUtil.get('map');
         if(container != null){
             container._leaflet_id = null;
         }
+        //initialize map lat and long of user
         var map = L.map('map', {
             center: [latitude, longtitude],
             zoom: 15
         });
-        
+        //add a tile layer to map
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+        //add a marker on that location with a custom icon
+        const locationIcon = L.icon({
+            iconUrl: 'icon-location.png', 
 
-        L.marker([latitude, longtitude]).addTo(map)
+        });
+        const marker = L.marker([latitude, longtitude], { icon: locationIcon }).addTo(map);
+        
     };
 });
 
-// make so that they can search up domain names as well
+// make so that they can search up domain names as well & add extra stuff
+
+
+
