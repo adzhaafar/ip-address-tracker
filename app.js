278af62dc9588a53ffa0357d9d2adcc7c542dc8e
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-03-20 10:16:15 +0200

    Functional map

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import { apiKey } from './apikey.js';
  
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    //selectors
     const ip = document.querySelector('.ip');
     const place = document.querySelector('.place');
     const time = document.querySelector('.time');
@@ -9,8 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector('#search');
     const searchBtn = document.querySelector('.search-btn');
     let ipAdress = "";
+
+    //user location & ip on first page load
     getData();
 
+    //event listeners
     searchBtn.addEventListener('click', () => {
         ipAdress = input.value;
         getData();
@@ -18,37 +23,35 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    //function to get info from apis
     function getData() {
-        fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAdress}`).then(res => res.json()).then(data => {
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAdress}`).then(res => res.json()).then(data => {
             ip.textContent = data.ip
             place.textContent = data.location.region
             time.textContent = data.location.timezone
             internet.textContent = data.isp
+            getMap(data.location.lat, data.location.lng)
+            // console.log(`[${data.location.lat}, ${data.location.lng}]`)
         });
     };
 
-    // map 
+    // function to get map location based on lat & long
+    function getMap(latitude, longtitude) { 
+        var container = L.DomUtil.get('map');
+        if(container != null){
+            container._leaflet_id = null;
+        }
+        var map = L.map('map', {
+            center: [latitude, longtitude],
+            zoom: 15
+        });
+        
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-    });
-
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>')
-        .openPopup();
-    
-    // var leadletIcon = L.icon({
-    //     iconUrl: '/images/icon-location.svg'
-
-    // });
-    
-    
+        L.marker([latitude, longtitude]).addTo(map)
+    };
 });
 
-// make so that they can search up domain names as well, add map
+// make so that they can search up domain names as well
