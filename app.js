561694927fd93facb7ca899ac798623c3608c19a
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-03-20 17:20:26 +0200

    Fixed responsive bugs, added popup, set functionality to return btn

--- app.js
+++ app.js
@@ -16,12 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
     getData();
 
     //event listeners
-    searchBtn.addEventListener('click', () => {
+    searchBtn.addEventListener('click', getResult);
+    // return on keyboard submits button 
+    document.addEventListener('keydown', logKey);
+
+
+    //main function triggered after clicking button 
+    function getResult() {
         ipAdress = input.value;
         getData();
         input.value = "";
-    });
-
+    }
+    //function triggered by click on return button
+    function logKey(event) {
+        if (event.keyCode === 13) {
+            getResult();
+        };
+    };
 
     //function to get info from apis
     function getData() {
@@ -32,12 +43,12 @@ document.addEventListener('DOMContentLoaded', () => {
             place.textContent = data.location.region;
             time.textContent = data.location.timezone;
             internet.textContent = data.isp;
-            getMap(data.location.lat, data.location.lng);
+            getMap(data.location.lat, data.location.lng, data.location.city, data.location.country);
         });
     };
 
     // function to get map location based on lat & long
-    function getMap(latitude, longtitude) { 
+    function getMap(latitude, longtitude, city, country) { 
         //check if map is already initialized, if yes set it to null
         var container = L.DomUtil.get('map');
         if(container != null){
@@ -58,11 +69,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
         });
         const marker = L.marker([latitude, longtitude], { icon: locationIcon }).addTo(map);
+
+        var latlng = L.latLng(latitude, longtitude);
+        //add a popup on the map with extra information
+        var popup = L.popup()
+        .setLatLng(latlng)
+        .setContent(`<p>City: ${city} (${country})<br />Latitude: ${latitude} <br/> Longtitude: ${longtitude}</p>`)
+        .openOn(map);
+        
+        
         
     };
+
+    //change the value of placeholder so it doesnt overflow when screen resizes
+    const placeholder = document.querySelector('#search');
+    let placeValue = placeholder.getAttribute("placeholder")
+
+    //selector
+    window.addEventListener('resize', changePlaceholder);
+
+    function changePlaceholder() {
+        let width = document.body.clientWidth;
+        if (width < 940) {
+            placeValue = "Search IP";
+            placeholder.setAttribute("placeholder", placeValue);
+        };
+    };
+
 });
 
-// make so that they can search up domain names as well & add extra stuff
-
-
-
+// make so that they can search up domain names as well    data.as.domain
+// fix the height issue
